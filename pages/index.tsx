60b3b72b95dd1783ee8370138451ddf1e3d003b9
hieu081199 <hieubait99@gmail.com>
2025-07-21 18:59:20 +0700

    raise max tokens

--- pages/index.tsx
+++ pages/index.tsx
@@ -881,7 +881,7 @@ export default function Home() {
           },
           { role: 'user', content: prompt },
         ],
-        max_tokens: 1500,
+        max_tokens: 2500,
         temperature: 0.6,
       });
 
