eb7f7d081decda6de8a3f61df837ad5319288ad2
hieu081199 <hieubait99@gmail.com>
2025-07-27 12:00:54 +0700

    fix

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         message: 'Please provide a collection name or configure QDRANT_COLLECTION in environment variables.'
       });
     }
-  
+    console.log(collection)
     const searchRes = await qdrantClient.search(collection, {
       vector: embedding,
       limit: limit || 5,
